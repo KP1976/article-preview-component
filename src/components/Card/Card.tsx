3f074d684367f71eb50014d4d49fa92cd6285bc0
Krzysztof Pawłowski <kpawlowski1976@gmail.com>
2024-12-15 11:47:52 +0100

    Import images directly to the component.

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,9 +1,12 @@
 import styles from "./Card.module.css";
+import drawerImage from "src/assets/images/drawers.jpg";
+import avatar from "src/assets/images/avatar-michelle.jpg";
+import shareIcon from "src/assets/images/icon-share.svg";
 
 const Card = () => {
   return (
     <article className={styles.card}>
-      <img src="./src/assets/images/drawers.jpg" alt="image-header" className={styles.image} />
+      <img src={drawerImage} alt="image-header" className={styles.image} />
       <div className={styles.content}>
         <header className={styles.header}>
           <h1 className={styles["header-title"]}>
@@ -17,18 +20,14 @@ const Card = () => {
         </header>
         <footer className={styles.footer}>
           <div className={styles.author}>
-            <img
-              src="./src/assets/images/avatar-michelle.jpg"
-              alt="Michelle Appleton"
-              className={styles["author-image"]}
-            />
+            <img src={avatar} alt="Michelle Appleton" className={styles["author-image"]} />
             <hgroup className={styles["author-info"]}>
               <h2 className={styles["author-name"]}>Michelle Appleton</h2>
               <h3 className={styles["author-date"]}>28 Jun 2020</h3>
             </hgroup>
           </div>
           <button className={styles["share-button"]}>
-            <img src="./src/assets/images/icon-share.svg" alt="share" />
+            <img src={shareIcon} alt="share" />
           </button>
         </footer>
       </div>
